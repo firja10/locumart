4e0b9ad23b6b786dd77b57624e2191354d56c8a4
firja10 <68978827+firja10@users.noreply.github.com>
2021-10-25 14:31:25 +0700

    save

--- resources/views/marketplace/template.blade.php
+++ resources/views/marketplace/template.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('judul')</title>
     <link rel="shortcut icon" href="{{asset('assets/logo-locumart.png')}}" type="image/x-icon">
-    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/base.css')}}" />
-    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/main.css')}}" />
-    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/grid.css')}}" />
-    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/responsive.css')}}" />
+    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/base.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/main.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/grid.css')}}" type="text/css" />
+    <link rel="stylesheet" href="{{asset('shopeeclone/assets/css/responsive.css')}}" type="text/css" />
     <link
       rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css')}}"
+      href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
     />
 
     @yield('scriptatas')
